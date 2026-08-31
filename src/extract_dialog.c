f4336923c62a6802c8697c2a27f8cd45399bd732
onesixromcom <16732145+onesixromcom@users.noreply.github.com>
2026-08-31 13:43:53 +0300

    Extract same folder option

--- src/extract_dialog.c
+++ src/extract_dialog.c
@@ -533,20 +533,31 @@ void xa_set_extract_dialog_options (ExtractDialog *extract_dialog, gint selected
 
 	if (!archive->destination_path)
 	{
-		gchar *archive_dir = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(prefs_window->preferred_extract_dir));
-
-		if (!archive_dir || !*archive_dir)
+		if (prefs_window->extract_same_folder)
 		{
-			gchar *archive_dir_utf8;
+			gchar *dirname = g_path_get_dirname(archive->path[0]);
 
-			g_free(archive_dir);
-			archive_dir_utf8 = g_filename_display_name(archive->path[0]);
-			archive_dir = xa_remove_level_from_path(archive_dir_utf8);
-			g_free(archive_dir_utf8);
+			archive->destination_path = g_strdup_printf("%s", dirname);
+
+			g_free(dirname);
 		}
+		else
+		{
+			gchar *archive_dir = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(prefs_window->preferred_extract_dir));
 
-		archive->destination_path = g_strdup(archive_dir);
-		g_free(archive_dir);
+			if (!archive_dir || !*archive_dir)
+			{
+				gchar *archive_dir_utf8;
+
+				g_free(archive_dir);
+				archive_dir_utf8 = g_filename_display_name(archive->path[0]);
+				archive_dir = xa_remove_level_from_path(archive_dir_utf8);
+				g_free(archive_dir_utf8);
+			}
+
+			archive->destination_path = g_strdup(archive_dir);
+			g_free(archive_dir);
+		}
 	}
 	gtk_entry_set_text(GTK_ENTRY(extract_dialog->destination_path_entry), archive->destination_path);
 	gtk_entry_set_text(GTK_ENTRY(extract_dialog->password_entry), archive->password ? archive->password : "");
